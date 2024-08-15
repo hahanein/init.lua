eda855ac27fe7352228a95bef877fa3848499720
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-15 22:48:47 +0200

    Initial commit

---
+++ lua/config/plug.lua
@@ -0,0 +1,199 @@
+local vim = vim ---@diagnostic disable-line
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'ctrlpvim/ctrlp.vim'
+Plug 'hahanein/vim-brutalism'
+
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = vim.fn[':TSUpdate'] })
+Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+
+Plug 'kylechui/nvim-surround'
+
+Plug 'vim-test/vim-test'
+Plug 'skywind3000/asyncrun.vim'
+
+Plug 'tpope/vim-fugitive'
+
+vim.call('plug#end')
+
+do -- Configure ripgrep:
+	vim.opt.grepprg = 'rg --color=never'
+	vim.g.ctrlp_user_command = 'rg %s --files --color=never --glob ""'
+	vim.g.ctrlp_use_caching = false
+end
+
+vim.cmd("colorscheme brutalism")
+
+require("nvim-surround").setup {}
+
+require("nvim-treesitter.configs").setup({
+	indent = { enable = true },
+	highlight = { enable = true },
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = '<c-space>',
+			node_incremental = '<c-space>',
+			scope_incremental = '<c-s>',
+			node_decremental = '<c-backspace>',
+		},
+	},
+
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true,
+			keymaps = {
+				['aa'] = '@parameter.outer',
+				['ia'] = '@parameter.inner',
+				['af'] = '@function.outer',
+				['if'] = '@function.inner',
+				['ac'] = '@class.outer',
+				['ic'] = '@class.inner',
+				['ii'] = '@conditional.inner',
+				['ai'] = '@conditional.outer',
+				['il'] = '@loop.inner',
+				['al'] = '@loop.outer',
+				['at'] = '@comment.outer',
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				[']f'] = '@function.outer',
+				[']]'] = '@class.outer',
+			},
+			goto_next_end = {
+				[']F'] = '@function.outer',
+				[']['] = '@class.outer',
+			},
+			goto_previous_start = {
+				['[f'] = '@function.outer',
+				['[['] = '@class.outer',
+			},
+			goto_previous_end = {
+				['[F'] = '@function.outer',
+				['[]'] = '@class.outer',
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				['<leader>a'] = '@parameter.inner',
+			},
+			swap_previous = {
+				['<leader>A'] = '@parameter.inner',
+			},
+		},
+	},
+})
+
+do -- Completion configuration:
+	local cmp = require 'cmp'
+
+	cmp.setup({
+		snippet = { expand = function(args) vim.snippet.expand(args.body) end },
+		mapping = cmp.mapping.preset.insert({
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete(),
+			['<C-e>'] = cmp.mapping.abort(),
+			['<CR>'] = cmp.mapping.confirm({ select = true }),
+		}),
+		sources = cmp.config.sources({ { name = 'nvim_lsp' } }, { { name = 'buffer' } })
+	})
+
+	cmp.setup.cmdline({ '/', '?' }, {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = { { name = 'buffer' } }
+	})
+
+	cmp.setup.cmdline(':', {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
+		matching = { disallow_symbol_nonprefix_matching = false }
+	})
+end
+
+do -- Language server configuration:
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	local servers = {
+		'clangd',
+		'rust_analyzer',
+		'tsserver',
+		'lua_ls',
+		'zls',
+		'bashls',
+		'cssls'
+	}
+
+	for _, lsp in ipairs(servers) do
+		require('lspconfig')[lsp].setup {
+			capabilities = capabilities,
+			on_attach = function(client, buffer)
+				do -- Miscellaneous:
+					client.server_capabilities.semanticTokensProvider = nil
+				end
+
+				do -- Remaps:
+					local opts = { buffer = buffer }
+					vim.keymap.set('n', 'g.', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', 'gs', vim.lsp.buf.document_symbol, opts)
+					vim.keymap.set('n', 'gS', vim.lsp.buf.workspace_symbol, opts)
+					vim.keymap.set('n', 'gA', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', 'cd', vim.lsp.buf.rename, opts)
+					vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+					vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+				end
+
+				do -- Format on save:
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						pattern = "*",
+						callback = function()
+							vim.lsp.buf.format { async = false }
+						end,
+					})
+				end
+
+				do -- Present diagnostics in floating window:
+					vim.diagnostic.config({ virtual_text = false, signs = false })
+					vim.o.updatetime = 250
+					vim.api.nvim_create_autocmd("CursorHold", {
+						callback = function()
+							vim.diagnostic.open_float(
+								nil,
+								{ focusable = false, scope = "cursor" }
+							)
+						end,
+					})
+				end
+			end
+		}
+	end
+end
+
+do -- Test configuration:
+	vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
+	vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
+	vim.keymap.set('n', '<leader>a', ':TestSuite<CR>')
+	vim.keymap.set('n', '<leader>l', ':TestLast<CR>')
+	vim.keymap.set('n', '<leader>g', ':TestVisit<CR>')
+
+	vim.g['test#strategy'] = 'asyncrun'
+	vim.g.asyncrun_open = 10
+end
---
+++ lua/config/set.lua
@@ -0,0 +1,34 @@
+vim.opt.backspace = "indent,eol,start" -- backspace deletes like most programs in insert mode
+vim.opt.history = 50
+vim.opt.ruler = true                   -- show the cursor position all the time
+vim.opt.showcmd = true                 -- display incomplete commands
+vim.opt.incsearch = true               -- do incremental searching
+vim.opt.laststatus = 2                 -- always display the status line
+vim.opt.autowrite = true               -- automatically :write before running commands
+vim.opt.hlsearch = true                -- highlight all search results
+vim.opt.tabstop = 4                    -- tab should be 4 spaces wide
+vim.opt.shiftwidth = 4
+
+vim.opt.numberwidth = 5
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.complete:append("kspell")  -- autocomplete with dictionary words when spell check is on
+vim.opt.diffopt:append("vertical") -- always use vertical diffs
+vim.opt.joinspaces = false         -- use one space, not two, after punctuation
+vim.opt.lazyredraw = true          -- redraw only when we need to
+vim.opt.wildmenu = true            -- visual autocomplete for command menu
+
+vim.opt.termguicolors = false
+vim.opt.completeopt = { "longest", "menuone" } -- make completion popup menu work like in an IDE
+
+vim.opt.backup = true
+vim.opt.backupdir = "/var/tmp"
+vim.opt.backupskip = "/var/tmp/*"
+vim.opt.directory = "/var/tmp"
+vim.opt.writebackup = true
+
+vim.opt.fillchars:append {
+	vert = '|', -- Vertical split separator
+	stl = '^', -- Horizontal split separator
+}
