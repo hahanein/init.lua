217f4aa183c58139f2361430168374dce3f6a081
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-17 15:30:39 +0200

    Style

--- lua/config/plug.lua
+++ lua/config/plug.lua
@@ -133,11 +133,11 @@ do -- Completion configuration:
 end
 
 do -- Language server configuration:
+	local capabilities = require('cmp_nvim_lsp').default_capabilities()
 	require("mason").setup()
 	require("mason-lspconfig").setup()
 	require("mason-lspconfig").setup_handlers {
 		function(server_name)
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 			require('lspconfig')[server_name].setup {
 				capabilities = capabilities,
 				on_attach = function(client, buffer)
