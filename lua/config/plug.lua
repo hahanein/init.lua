71e367042e0f495942e3df78aec834c9e10c21a5
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-17 15:27:00 +0200

    Set up LSP automatically, also formatting

--- lua/config/plug.lua
+++ lua/config/plug.lua
@@ -1,29 +1,33 @@
 local vim = vim ---@diagnostic disable-line
 local Plug = vim.fn['plug#']
 
-vim.call('plug#begin')
+do -- Import plugins:
+	vim.call('plug#begin')
 
-Plug 'ctrlpvim/ctrlp.vim'
-Plug 'hahanein/vim-brutalism'
+	Plug 'ctrlpvim/ctrlp.vim'
+	Plug 'hahanein/vim-brutalism'
 
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = vim.fn[':TSUpdate'] })
-Plug 'nvim-treesitter/nvim-treesitter-textobjects'
+	Plug('nvim-treesitter/nvim-treesitter', { ['do'] = vim.fn[':TSUpdate'] })
+	Plug 'nvim-treesitter/nvim-treesitter-textobjects'
 
-Plug 'neovim/nvim-lspconfig'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
+	Plug 'williamboman/mason.nvim'
+	Plug 'williamboman/mason-lspconfig.nvim'
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'hrsh7th/cmp-buffer'
+	Plug 'hrsh7th/cmp-path'
+	Plug 'hrsh7th/cmp-cmdline'
+	Plug 'hrsh7th/nvim-cmp'
 
-Plug 'kylechui/nvim-surround'
+	Plug 'kylechui/nvim-surround'
 
-Plug 'vim-test/vim-test'
-Plug 'skywind3000/asyncrun.vim'
+	Plug 'vim-test/vim-test'
+	Plug 'skywind3000/asyncrun.vim'
 
-Plug 'tpope/vim-fugitive'
+	Plug 'tpope/vim-fugitive'
 
-vim.call('plug#end')
+	vim.call('plug#end')
+end
 
 do -- Configure ctrlp:
 	vim.opt.grepprg = 'rg --color=never'
@@ -129,65 +133,59 @@ do -- Completion configuration:
 end
 
 do -- Language server configuration:
-	local capabilities = require('cmp_nvim_lsp').default_capabilities()
-	local servers = {
-		'clangd',
-		'rust_analyzer',
-		'tsserver',
-		'lua_ls',
-		'zls',
-		'bashls',
-		'cssls'
+	require("mason").setup()
+	require("mason-lspconfig").setup()
+	require("mason-lspconfig").setup_handlers {
+		function(server_name)
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require('lspconfig')[server_name].setup {
+				capabilities = capabilities,
+				on_attach = function(client, buffer)
+					do -- Miscellaneous:
+						client.server_capabilities.semanticTokensProvider = nil
+					end
+
+					do -- Remaps:
+						local opts = { buffer = buffer }
+						vim.keymap.set('n', 'g.', vim.lsp.buf.code_action, opts)
+						vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+						vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+						vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+						vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
+						vim.keymap.set('n', 'gs', vim.lsp.buf.document_symbol, opts)
+						vim.keymap.set('n', 'gS', vim.lsp.buf.workspace_symbol, opts)
+						vim.keymap.set('n', 'gA', vim.lsp.buf.references, opts)
+						vim.keymap.set('n', 'cd', vim.lsp.buf.rename, opts)
+						vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+						vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+						vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+					end
+
+					do -- Format on save:
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							pattern = "*",
+							callback = function()
+								vim.lsp.buf.format { async = false }
+							end,
+						})
+					end
+
+					do -- Present diagnostics in floating window:
+						vim.diagnostic.config({ virtual_text = false, signs = false })
+						vim.o.updatetime = 250
+						vim.api.nvim_create_autocmd("CursorHold", {
+							callback = function()
+								vim.diagnostic.open_float(
+									nil,
+									{ focusable = false, scope = "cursor" }
+								)
+							end,
+						})
+					end
+				end
+			}
+		end
 	}
-
-	for _, lsp in ipairs(servers) do
-		require('lspconfig')[lsp].setup {
-			capabilities = capabilities,
-			on_attach = function(client, buffer)
-				do -- Miscellaneous:
-					client.server_capabilities.semanticTokensProvider = nil
-				end
-
-				do -- Remaps:
-					local opts = { buffer = buffer }
-					vim.keymap.set('n', 'g.', vim.lsp.buf.code_action, opts)
-					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-					vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
-					vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, opts)
-					vim.keymap.set('n', 'gs', vim.lsp.buf.document_symbol, opts)
-					vim.keymap.set('n', 'gS', vim.lsp.buf.workspace_symbol, opts)
-					vim.keymap.set('n', 'gA', vim.lsp.buf.references, opts)
-					vim.keymap.set('n', 'cd', vim.lsp.buf.rename, opts)
-					vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-					vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
-					vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
-				end
-
-				do -- Format on save:
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						pattern = "*",
-						callback = function()
-							vim.lsp.buf.format { async = false }
-						end,
-					})
-				end
-
-				do -- Present diagnostics in floating window:
-					vim.diagnostic.config({ virtual_text = false, signs = false })
-					vim.o.updatetime = 250
-					vim.api.nvim_create_autocmd("CursorHold", {
-						callback = function()
-							vim.diagnostic.open_float(
-								nil,
-								{ focusable = false, scope = "cursor" }
-							)
-						end,
-					})
-				end
-			end
-		}
-	end
 end
 
 do -- Test configuration:
