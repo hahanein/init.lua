9c69d40ab0290ca85b86abcd53f89d444caabfe7
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-17 16:01:46 +0200

    Add keymap to navigate native nvim snippets

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,2 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+-- Maps <Tab> to jump while a snippet is active.
+-- For more information see: https://neovim.io/doc/user/lua.html#vim.snippet
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+	if vim.snippet.active({ direction = 1 }) then
+		return '<Cmd>lua vim.snippet.jump(1)<CR>'
+	else
+		return '<Tab>'
+	end
+end, { expr = true })
