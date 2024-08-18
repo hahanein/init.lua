d56e127a4015b3ecf79a4e0da5b31fa7da6b8d29
Benjamin Westphal <hahanein@users.noreply.github.com>
2024-08-18 12:02:29 +0200

    Fix call to non-existant formatter

--- lua/config/plug.lua
+++ lua/config/plug.lua
@@ -225,7 +225,9 @@ on_event_once("BufWritePost", { -- Formatter configuration:
 		filetype["*"] = { -- Fallback on LSP formatting if available:
 			require("formatter.filetypes.any").remove_trailing_whitespace,
 			function()
-				if filetype[vim.bo.filetype] == nil and vim.lsp.buf.format ~= nil then
+				local bufnr = vim.api.nvim_get_current_buf()
+				local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+				if filetype[vim.bo.filetype] == nil and #clients > 0 then
 					vim.lsp.buf.format({ async = false })
 				end
 			end,
