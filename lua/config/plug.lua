baa7a35a10c46230fb1185ddad39620f4218c579
Benjamin Westphal <hahanein@users.noreply.github.com>
2025-05-08 12:42:58 +0200

    Quickly select files

--- lua/config/plug.lua
+++ lua/config/plug.lua
@@ -289,6 +289,11 @@ do -- Harpoon configuration:
 	vim.keymap.set("n", "<C-e>", function()
 		harpoon.ui:toggle_quick_menu(harpoon:list())
 	end)
+	for i = 1, 9 do
+		vim.keymap.set("n", string.format("<C-%d>", i), function()
+			harpoon:list():select(i)
+		end)
+	end
 end
 
 require("minuet").setup({
