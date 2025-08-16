2a5396ae314b2897302fef5e17c0b02db3ab7eaa
hahanein <binjamin@riseup.net>
2025-08-16 14:26:56 +0200

    Use generic path

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,5 @@
 require("jdtls").start_or_attach({
-	cmd = { "/home/benjamin/.local/share/nvim/mason/bin/jdtls" },
+	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
 	root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml" }, { upward = true })[1]),
 	on_attach = require("config.lsp").on_attach,
 	settings = {
