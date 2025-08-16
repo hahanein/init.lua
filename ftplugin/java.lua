9034d94f5c88feb2a83c6aaecf2a8ccc85828768
hahanein <binjamin@riseup.net>
2025-08-16 14:32:31 +0200

    Remove superfluous block

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,16 +3,15 @@ require("jdtls").start_or_attach({
 	root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml" }, { upward = true })[1]),
 	on_attach = function(client, buffer)
 		require("config.lsp").on_attach(client, buffer)
-		if client.name == "jdtls" then -- Enable Eclipse LSP Formatting for Java:
-			local grp = vim.api.nvim_create_augroup("JdtlsFormat", { clear = true })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = grp,
-				buffer = buffer,
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
+
+		-- Enable Eclipse LSP Formatting for Java:
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = vim.api.nvim_create_augroup("JdtlsFormat", { clear = true }),
+			buffer = buffer,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
 	end,
 	settings = {
 		java = {
