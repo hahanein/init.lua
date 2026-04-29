1bbac5337a506dfc8c953fa95ed2ca389edb3c74
Benjamin Westphal <benjamin.westphal@macmon.eu>
2026-04-29 17:05:38 +0200

    Fix Java formatter not running

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -21,6 +21,21 @@ require("jdtls").start_or_attach({
 					profile = "macmon-java",
 				},
 			},
+			configuration = {
+				-- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+				-- And search for `interface RuntimeOption`
+				-- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
+				runtimes = {
+					{
+						name = "JavaSE-17",
+						path = "/usr/lib/jvm/java-17-amazon-corretto/",
+					},
+					{
+						name = "JavaSE-21",
+						path = "/usr/lib/jvm/java-21-amazon-corretto/",
+					},
+				},
+			},
 		},
 	},
 })
